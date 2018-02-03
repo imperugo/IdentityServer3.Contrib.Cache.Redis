0b4fb33c864d43549dff94e612a51caaadcb374f
Ma3yTa <maksimetsa@ukr.net>
2018-02-03 17:32:25 +0700

    fx

--- src/IdentityServer3.Contrib.Cache.Redis/RedisAuthorizationCodeStore.cs
+++ src/IdentityServer3.Contrib.Cache.Redis/RedisAuthorizationCodeStore.cs
@@ -1,36 +1,35 @@
-﻿using IdentityServer3.Core.Models;
-using IdentityServer3.Core.Services;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using IdentityServer3.Contrib.Cache.Redis.CacheClient;
+using IdentityServer3.Core.Models;
+using IdentityServer3.Core.Services;
+using StackExchange.Redis;
 
 namespace IdentityServer3.Contrib.Cache.Redis
 {
     public class RedisAuthorizationCodeStore : IAuthorizationCodeStore
     {
+        private readonly ICacheManager cacheClient;
+
+        public RedisAuthorizationCodeStore(ConnectionMultiplexer connection)
+            : this(new RedisCacheManager(connection)) { }
+
+        public RedisAuthorizationCodeStore(ICacheManager cacheClient)
+            => this.cacheClient = cacheClient;
+
         public Task<IEnumerable<ITokenMetadata>> GetAllAsync(string subject)
-        {
-            throw new NotImplementedException();
-        }
+            => throw new System.NotImplementedException();
 
         public Task<AuthorizationCode> GetAsync(string key)
-        {
-            throw new NotImplementedException();
-        }
+            => cacheClient.GetAsync<AuthorizationCode>(key);
 
         public Task RemoveAsync(string key)
-        {
-            throw new NotImplementedException();
-        }
+            => cacheClient.RemoveAsync(key);
 
         public Task RevokeAsync(string subject, string client)
-        {
-            throw new NotImplementedException();
-        }
+            => throw new System.NotImplementedException();
 
         public Task StoreAsync(string key, AuthorizationCode value)
-        {
-            throw new NotImplementedException();
-        }
+            => cacheClient.AddAsync(key, value);
     }
 }
--- src/IdentityServer3.Contrib.Cache.Redis/RedisRefreshTokenStore.cs
+++ src/IdentityServer3.Contrib.Cache.Redis/RedisRefreshTokenStore.cs
@@ -1,5 +1,7 @@
-﻿using IdentityServer3.Core.Models;
+﻿using IdentityServer3.Contrib.Cache.Redis.CacheClient;
+using IdentityServer3.Core.Models;
 using IdentityServer3.Core.Services;
+using StackExchange.Redis;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -8,29 +10,27 @@ namespace IdentityServer3.Contrib.Cache.Redis
 {
     public class RedisRefreshTokenStore : IRefreshTokenStore
     {
+        private readonly ICacheManager cacheClient;
+
+        public RedisRefreshTokenStore(ConnectionMultiplexer connection)
+            : this(new RedisCacheManager(connection)) { }
+
+        public RedisRefreshTokenStore(ICacheManager cacheClient)
+            => this.cacheClient = cacheClient;
+
         public Task<IEnumerable<ITokenMetadata>> GetAllAsync(string subject)
-        {
-            throw new NotImplementedException();
-        }
+            => throw new NotImplementedException();
 
         public Task<RefreshToken> GetAsync(string key)
-        {
-            throw new NotImplementedException();
-        }
+            => cacheClient.GetAsync<RefreshToken>(key);
 
         public Task RemoveAsync(string key)
-        {
-            throw new NotImplementedException();
-        }
+            => cacheClient.RemoveAsync(key);
 
         public Task RevokeAsync(string subject, string client)
-        {
-            throw new NotImplementedException();
-        }
+            => throw new NotImplementedException();
 
         public Task StoreAsync(string key, RefreshToken value)
-        {
-            throw new NotImplementedException();
-        }
+            => cacheClient.AddAsync(key, value);
     }
 }
