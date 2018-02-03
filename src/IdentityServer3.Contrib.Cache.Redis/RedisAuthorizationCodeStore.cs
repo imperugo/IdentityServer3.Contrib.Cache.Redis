51f846ef0a3b0c745fd089543146d340956aa265
Ma3yTa <maksimetsa@ukr.net>
2018-02-03 17:15:05 +0700

    fx

---
+++ src/IdentityServer3.Contrib.Cache.Redis/RedisAuthorizationCodeStore.cs
@@ -0,0 +1,36 @@
+﻿using IdentityServer3.Core.Models;
+using IdentityServer3.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace IdentityServer3.Contrib.Cache.Redis
+{
+    public class RedisAuthorizationCodeStore : IAuthorizationCodeStore
+    {
+        public Task<IEnumerable<ITokenMetadata>> GetAllAsync(string subject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AuthorizationCode> GetAsync(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveAsync(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RevokeAsync(string subject, string client)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StoreAsync(string key, AuthorizationCode value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/IdentityServer3.Contrib.Cache.Redis/RedisRefreshTokenStore.cs
@@ -0,0 +1,36 @@
+﻿using IdentityServer3.Core.Models;
+using IdentityServer3.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace IdentityServer3.Contrib.Cache.Redis
+{
+    public class RedisRefreshTokenStore : IRefreshTokenStore
+    {
+        public Task<IEnumerable<ITokenMetadata>> GetAllAsync(string subject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<RefreshToken> GetAsync(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveAsync(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RevokeAsync(string subject, string client)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StoreAsync(string key, RefreshToken value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
